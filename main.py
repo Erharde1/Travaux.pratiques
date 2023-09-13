bb080cc446148029e15da472f43aa442f9698b0b
Erharde1 <143745983+Erharde1@users.noreply.github.com>
2023-09-13 12:04:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,54 @@
-#TP1
-#compter le nombre de mots d'une chaine
+print("""Bonjour! Bienvenue dans le langage python!
+      Voici les choix pour le programme:
+      \t1-Jouer
+      \t2-Afficher les règles
+      \t3-Quitter
+      \t Quel est votre choix?""""")
 
-#définir la chaine
-chaine="la fonction devra retourner le nombre de mots"
-#imprimer la chaine
-print("La chaine de charactères est: "+chaine)
-#utiliser split pour compter le nombre de mots
-count_word=len(chaine.split())
-#imprimer le nombre de mots
-print("Le nombre de mots est: "+str(count_word))
+
+import random
+#demander à l'utilisateur un string et le print
+#nom=input("Quel est votre nom?")
+#print("Bonjour "+nom+". Comment allez-vous?")
+
+#def addition(entier1, parametre2):
+  #  somme = entier1+parametre2
+ #   return somme
+
+
+#print(addition(2,3))
+
+
+#def division(numerateur, denominateur):
+#    if denominateur==0:
+#        return -1
+#    div=numerateur/denominateur
+#    return div
+
+
+
+
+
+#exercice 4
+
+
+#nbr_de_nombres_impairs=0
+#nbr_de_nombres_pairs=0
+
+#for chiffre in range(1,10):
+#    if (chiffre % 2) == 0:
+#        print(str(chiffre) + " est pair.")
+ #       nbr_de_nombres_pairs += 1
+#
+ #   else:
+#        print(str(chiffre) + " est impair.")
+#        nbr_de_nombres_impairs +=1
+
+#print("Le nombre de chiffres pairs est:"+str(nbr_de_nombres_pairs))
+#print("Le nombre de chiffres impairs est:"+str(nbr_de_nombres_impairs))
+
+
+
+#pour choisir un nombre aléatoire
+
+print(random.randint(1,7))
